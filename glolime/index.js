0be7fb20460101a105baed79736edb9141677d58
mikhaillav <mikhaillavelin@gmail.com>
2023-04-19 17:11:28 +0300

    change html parser lib, fix bugs

--- glolime/index.js
+++ glolime/index.js
@@ -5,31 +5,59 @@
 */
 
 const axios = require('axios')
-const cheerio = require('cheerio')
+const parse = require('node-html-parser')
 const iconv = require('iconv-lite');
 
 class glolime {
 
     /**
-     * Запрос на school.glolime.ru/acquiring/searchbypaymentnumber/acquier/
+     * Запрос на school.glolime.ru/api/netrika/userinfo/
      * 
-     * Возвращает ссылку на оплату 
-     * @param {Number} type 1 - горячее, 2 - буфет
-     * @param {Number} usernumber 
+     * Возвращает номер счета
+     * 
+     * @param {Number} merch_gmt 
+     * @param {String} merch_url 
+     * @param {String} nonce 
+     * @param {Number} suid
+     * @param {Number} timestamp 
+     * @param {String} token
+     * @param {String} zsign
     */
-    getNumberID_frameData(frameData){
+        getAccountID(frameData){
+            return axios.get(`https://school.glolime.ru/api/netrika/userinfo/?${frameData}`,{ responseType: 'arraybuffer' })
+            .then(response => {    
+                data = iconv.decode(response.data, 'windows-1251');
+    
+                const root = parse.parse(data)
+                const str = Number(root.querySelectorAll("td")[0].childNodes[0]._rawText)
+    
+                return str
+            })
+            .catch(error => {
+                console.log(error)
+                throw "Something went wrong by getting school.glolime.ru/api/netrika/userinfo/ via frameData";
+            });
+        }
+
+    /**
+     * Запрос на school.glolime.ru/api/netrika/userinfo/
+     * 
+     * Возвращает номер счета
+     * @param {String} frameData 
+    */
+    getAccountID_frameData(frameData){
         return axios.get(`https://school.glolime.ru/api/netrika/userinfo/?${frameData}`,{ responseType: 'arraybuffer' })
         .then(response => {    
             data = iconv.decode(response.data, 'windows-1251');
 
-            const balance = cheerio.load(data);
-            let str = balance('.document-list').find(balance('td'))[0].children[0].data
-            
-            return str  
+            const root = parse.parse(data)
+            const str = Number(root.querySelectorAll("td")[0].childNodes[0]._rawText)
+
+            return str
         })
         .catch(error => {
             console.log(error)
-            throw "Something went wrong by getting school.glolime.ru/api/netrika/transfer/create/";
+            throw "Something went wrong by getting school.glolime.ru/api/netrika/userinfo/ via frameData";
         });
     }
 
@@ -40,7 +68,7 @@ class glolime {
      * @param {Number} type 1 - горячее, 2 - буфет
      * @param {Number} usernumber 
     */
-    addMoney(type, usernumber){
+    getAddMoneyString(type, usernumber){
         return `https://school.glolime.ru/acquiring/searchbypaymentnumber/acquier/?type=${type}&usernumber=${usernumber}`
     }
 
@@ -101,16 +129,16 @@ class glolime {
         .then(response => {
             data = iconv.decode(response.data, 'windows-1251');
 
-            const balance = cheerio.load(data);
-            let str = balance('.document-list').find(balance('tbody')).find(balance('.textalign-right'))
+            const root = parse.parse(data)
+            const str = root.querySelectorAll("tbody")[0]
 
             return {
-                hot_feed: str[0].children[0].data,
-                cafeteria: str[1].children[0].data,
-                card_update: str[2].children[0].data,
-                sms_warns: str[3].children[0].data,
-                bracelet: str[4].children[0].data,
-                mobile_app: str[5].children[0].data
+                hot_feed:    Number(str.childNodes[1].childNodes[1].rawText),
+                cafeteria:   Number(str.childNodes[2].childNodes[1].rawText),
+                card_update: Number(str.childNodes[3].childNodes[1].rawText),
+                sms_warns:   Number(str.childNodes[4].childNodes[1].rawText),
+                bracelet:    Number(str.childNodes[5].childNodes[1].rawText),
+                mobile_app:  Number(str.childNodes[6].childNodes[1].rawText)
             }  
         })
         .catch(error => {
@@ -130,16 +158,16 @@ class glolime {
         .then(response => {
             data = iconv.decode(response.data, 'windows-1251');
 
-            const balance = cheerio.load(data);
-            let str = balance('.document-list').find(balance('tbody')).find(balance('.textalign-right'))
+            const root = parse.parse(data)
+            const str = root.querySelectorAll("tbody")[0]
 
             return {
-                hot_feed: str[0].children[0].data,
-                cafeteria: str[1].children[0].data,
-                card_update: str[2].children[0].data,
-                sms_warns: str[3].children[0].data,
-                bracelet: str[4].children[0].data,
-                mobile_app: str[5].children[0].data
+                hot_feed:    Number(str.childNodes[1].childNodes[1].rawText),
+                cafeteria:   Number(str.childNodes[2].childNodes[1].rawText),
+                card_update: Number(str.childNodes[3].childNodes[1].rawText),
+                sms_warns:   Number(str.childNodes[4].childNodes[1].rawText),
+                bracelet:    Number(str.childNodes[5].childNodes[1].rawText),
+                mobile_app:  Number(str.childNodes[6].childNodes[1].rawText)
             }  
         })
         .catch(error => {
